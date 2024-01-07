f3b2cdeba48abc9fb402517f4affcfdfb2c1541b
20190965YoungJin <dudwls1564954@naver.com>
2024-01-07 18:42:21 +0900

    스테이터스 연동 완료, 스탯창 제작

--- SideProject_MapleStroy/Assets/02.Scripts/YJScript/StatusManager.cs
+++ SideProject_MapleStroy/Assets/02.Scripts/YJScript/StatusManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+//using static System.Net.Mime.MediaTypeNames;
 
 namespace XEntity.InventoryItemSystem
 {
@@ -10,20 +11,34 @@ namespace XEntity.InventoryItemSystem
     {
         public static StatusManager Instance { get; private set; }
 
-        public float hp = 50;
-        public float mp = 30;
+        //스테이터스
+        private float maxHP = 50;
+        public float currentHP = 50;
+        private float maxMP = 30;
+        public float currentMP = 30;
         public int level = 1;
         public float currentEXP = 0; //현재 경험치
         private float exp; //총 경험치량
 
-        public int stat_STR; //능력치
+        //능력치
+        public int stat_STR;
         public int stat_DEX;
         public int stat_LUK;
         public int stat_INT;
+        
+        private float LvPercent; //경험치 백분율 계산
 
-        public Image expBar; //경험치 바
-        public TextMeshProUGUI LvText; //레벨 표시
-        public TextMeshProUGUI LvPersent; //경험치바 %표시
+        //텍스트
+        private TextMeshProUGUI HPText;
+        private TextMeshProUGUI MPText;
+        private TextMeshProUGUI LvText; //레벨 표시
+        private TextMeshProUGUI LvPercentText; //경험치바 %표시
+
+        //이미지 바
+        public Image hpBar;
+        public Image mpBar;
+        public Image expBar;
+
 
         void Awake()
         {
@@ -43,12 +58,19 @@ namespace XEntity.InventoryItemSystem
 
         void Start()
         {
+            HPText = GameObject.Find("HPText").GetComponent<TextMeshProUGUI>();
+            MPText = GameObject.Find("MPText").GetComponent<TextMeshProUGUI>();
+            LvText = GameObject.Find("LvText").GetComponent<TextMeshProUGUI>();
+            LvPercentText = GameObject.Find("LvPercentText").GetComponent<TextMeshProUGUI>();
+
             CalculateEXP();
+            StatusUP();
+            Die();
         }
 
         void Update()
         {
-
+            UpdateStatus();
         }
 
         void CalculateEXP()// 필요 경험치 계산
@@ -76,12 +98,42 @@ namespace XEntity.InventoryItemSystem
             }
 
             LvText.text = level.ToString(); //레벨 동기화
+            LvPercent = currentEXP / exp * 100f; //경험치 백분율 계산
+            LvPercentText.text = currentEXP.ToString() + " [ " + LvPercent.ToString() + "% ]"; //경험치바 %표시
+
+            StatusUP();
         }
 
         void UpdateExpBar()// 경험치바 업데이트
         {
             LevelUP();
             expBar.fillAmount = currentEXP / exp;
+        }
+
+        void UpdateStatus() //나중에 HP,MP 감소하는 함수 구현시 Update문에서 빼고 그 함수로 이동 예정
+        {
+            hpBar.fillAmount = currentHP / maxHP;
+            mpBar.fillAmount = currentMP / maxMP;
+            HPText.text = currentHP.ToString() + " / " + maxHP.ToString();
+            MPText.text = currentMP.ToString() + " / " + maxMP.ToString();
+        }
+
+        void StatusUP() //HP, MP 레벨 증가 및 초기화
+        {
+            maxHP += level * 50;
+            currentHP = maxHP;
+
+            maxMP += level * 40;
+            currentMP = maxMP;
+        }
+
+        void Die()
+        {
+            if(currentHP <= 0)
+            {
+                // 경험치 30% 감소
+                currentEXP = currentEXP * 0.7f;
+            }
         }
     }
 }
